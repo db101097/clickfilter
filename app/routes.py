e05b018c3e86857d316df936851d84b63ddec918
kre64 <kre9404@gmail.com>
2018-10-03 20:15:14 -0400

    basic stuff

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+#routes.py
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('index.html')
---
+++ app/static/js/main.js
@@ -0,0 +1,11 @@
+function showPicture(event){
+    var file = event.target.files[0]
+    var reader = new FileReader()
+    reader.onload = function(e){
+        var img = document.getElementById("myimage")
+        img.title = file.name
+        img.src = e.target.result
+        document.body.appendChild(image)
+    };
+    reader.readAsDataURL(file)
+}
