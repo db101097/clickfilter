249bd803f7c5f5b1e7d84e015a15e978befd4ffa
kre64 <kre9404@gmail.com>
2018-10-16 14:22:14 -0400

    extra filters added

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 # routes.py
 import base64
+import random
 import re
 import PIL
 from flask import render_template, jsonify, request, send_file, make_response
 from app import app
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageEnhance
 from io import BytesIO
 
 # to be used if we need to save files to server.
@@ -45,6 +46,36 @@ def filterimg():
         img.filter(ImageFilter.EMBOSS).save(img_io, 'PNG', quality=70)
     elif value == 'edge':
         img.filter(ImageFilter.EDGE_ENHANCE_MORE).save(img_io, 'PNG', quality=70)
+    elif value == 'noir':
+        img = PIL.ImageEnhance.Sharpness(img).enhance(1.3)
+        img = PIL.ImageEnhance.Color(img).enhance(1.5)
+        img = PIL.ImageEnhance.Contrast(img).enhance(1.1)
+        img = PIL.ImageEnhance.Brightness(img).enhance(1.1)
+        img.convert(mode='L').save(img_io, 'PNG', quality=70)
+        img.save(img_io, 'PNG', quality=70)
+    elif value == 'juicy':
+        img = PIL.ImageEnhance.Color(img).enhance(2.0)  # oversaturate
+        img = PIL.ImageEnhance.Contrast(img).enhance(1.2)  # oversaturate
+        img.save(img_io, 'PNG', quality=70)
+    elif value == 'slumber':
+        img = PIL.ImageEnhance.Color(img).enhance(0.5)
+        source = img.split()
+        R, G, B = 0, 1, 2
+
+        mask = source[B].point(lambda i: i < 60 and 255)
+        out = source[R].point(lambda i: i * 1.2)
+        source[R].paste(out, None, mask)
+
+        out = source[G].point(lambda i: i * 1.2)
+        source[G].paste(out, None, mask)
+
+        out = source[B].point(lambda i: i * 0.5)
+        source[B].paste(out, None, mask)
+
+        img = Image.merge(img.mode, source)
+        img = PIL.ImageEnhance.Color(img).enhance(1.4)
+        img = PIL.ImageEnhance.Contrast(img).enhance(1.2)
+        img.save(img_io, 'PNG', quality=70)
 
     # set img_io HEAD to first position.
     img_io.seek(0)
