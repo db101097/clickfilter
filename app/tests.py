5cbe89305d6a307d1b62b26e3e8b9cd78ec05765
kre64 <kre9404@gmail.com>
2018-10-31 19:57:46 -0400

    working my way through a simple test example

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,16 @@
 # tests.py
 # todo: Add the tests!
 
-from app import routes.py
+from app import routes
+
+class FilterApp:
+    def setup(self):
+        self.filter_value = "test"
+        self.img_string = "teststring"
+
+def test_filter_value_is_valid(self):
+    self.setup()
+
+
+def test_img_string_is_decoded(self):
+    self.setup()
