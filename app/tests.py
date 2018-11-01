8ab93898f49106dea99861708669f90bccf8a7c3
kre64 <kre9404@gmail.com>
2018-10-31 20:11:45 -0400

    setting up tests for mocker use

--- app/tests.py
+++ app/tests.py
@@ -4,14 +4,19 @@
 import sys
 sys.path.append("../")
 from app import routes
+from PIL import Image
 
 class FilterApp:
     def setup(self):
         self.filter_value = "test"
         self.img_string = "teststring"
 
+    # Test for the imagefilter value
     def test_filter_value_is_valid(self):
         self.setup()
 
+    # Test the image string
     def test_img_string_is_decoded(self):
         self.setup()
+
+    # Test the image was properly encoded?
