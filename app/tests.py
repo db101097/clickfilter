3eb94cf1b902eeaa8cf06ff12a1312f5c06353b6
kre64 <kre9404@gmail.com>
2018-10-31 20:02:21 -0400

    indenting, indenting, indenting

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,8 @@
 # tests.py
 # todo: Add the tests!
 
+import sys
+sys.path.append("../")
 from app import routes
 
 class FilterApp:
@@ -8,9 +10,8 @@ class FilterApp:
         self.filter_value = "test"
         self.img_string = "teststring"
 
-def test_filter_value_is_valid(self):
-    self.setup()
+    def test_filter_value_is_valid(self):
+        self.setup()
 
-
-def test_img_string_is_decoded(self):
-    self.setup()
+    def test_img_string_is_decoded(self):
+        self.setup()
