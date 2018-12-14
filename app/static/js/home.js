4f370d93d26c14c53308192ae8831858e9ef2cd9
kre64 <krecoding@gmail.com>
2018-12-14 15:19:29 -0500

    modals added for login/signup

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -7,3 +7,13 @@ $('#closeDrawer').click(function() {
     console.log("drawer closed")
     document.getElementById("mySidenav").style.width = "0";
 });
+
+$('#login').on('show.bs.modal', function (event) {
+    var modal = $(this)
+    modal.find('.modal-title').text('Adding a new album..')
+})
+
+$('#signup').on('show.bs.modal', function (event) {
+    var modal = $(this)
+    modal.find('.modal-title').text('Adding a new album..')
+  })
